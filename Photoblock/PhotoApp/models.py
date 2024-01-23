19d1e35821c125d9008e1eadfabc75ad76d36506
Z3VR4 <soyalexisg@gmail.com>
2024-01-23 10:06:35 +0100

    Created models, haven't run migrations yet.

--- Photoblock/PhotoApp/models.py
+++ Photoblock/PhotoApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    short_description = models.TextField(max_length=255)
+    social_links = models.TextField(blank=True)
+
+class ImageSubmission(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='image_submissions/')
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    image_submission = models.ForeignKey(ImageSubmission, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
