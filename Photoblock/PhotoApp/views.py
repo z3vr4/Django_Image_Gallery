cf821b9283bb99b5e4684bad0cf1e2072ddea733
Z3VR4 <soyalexisg@gmail.com>
2024-01-30 20:52:23 +0100

    Registration view and form addition

--- Photoblock/PhotoApp/views.py
+++ Photoblock/PhotoApp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from .models import UserProfile
 
 # Create your views here.
 
@@ -17,15 +18,44 @@ def upload_view(request):
 
 
 # Login will be handled by built-in django view, so I think this is now useless. I commented it out Lul
-
 #def login_view(request):
 #    return render(request, 'PhotoApp/login.html')
 
 
-# Come back to this to see how it works exactly, okay?
+# 30/01/2024 - Modified this view. Severely. Should create a UserProfile instance AND create a User instance. Still haven't tested anything. Likely to blow up.
+
 def registration_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Save User
+            user = form.save()
+
+            # Create UserProfile instance
+            UserProfile.objects.create(
+                user=user,
+                profile_image=request.POST.get('profile_image', ''),  # Update with the actual field names
+                short_description=request.POST.get('short_description', ''),
+                social_links=request.POST.get('social_links', ''),
+                creation_date =request.POST.get('creation_date'),
+            )
+            # Log the user in (Optional)
+            login(request, user)
+
+            return redirect('main')  # Replace 'main' with your actual redirect URL
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'PhotoApp/registration.html', {'form': form})
+
+
+
+
+# Come back to this to see how it works exactly, okay?
+'''
+def registration_view(request):
+    if request.method == 'POST':
+       form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -34,3 +64,4 @@ def registration_view(request):
         form = UserCreationForm()
 
     return render(request, 'PhotoApp/registration.html', {'form': form})
+'''
